c24f8862a0d8d9cf5f73d35a6b7c5a662a6bf6ab
achira7 <achira.sanjana@gmail.com>
2025-01-21 21:08:27 +0530

    Design Page Thumbnail layout

--- frontend/src/components/Thumbnail.jsx
+++ frontend/src/components/Thumbnail.jsx
@@ -1,17 +1,17 @@
 const Thumbnail = ({ img, title, description }) => {
   return (
     <div className="flex justify-center items-center">
-            <div className="relative w-[20vh] rounded-3xl overflow-hidden group cursor-pointer">
+            <div className="relative w-[30vh] rounded-3xl overflow-hidden group cursor-pointer m-0">
               {/* Image */}
-              <img src={img} alt={title} className="w-full " />
+              <img src={img} alt={title} className="w-full" />
     
               {/* Overlay */}
-              <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-60 transition duration-700 flex items-end">
+              <div className="absolute flex inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-60 transition duration-700 items-end">
                 <div className="text-white px-6 py-5 transform translate-y-full group-hover:translate-y-0 transition duration-700">
                   <h2 className="text-2xl font-secondary font-semibold tracking-wide capitalize my-2">
                     {title}
                   </h2>
-                  <p className="text-lg font-body">
+                  <p className="text-lg font-body hidden md:flex">
                     {description}
                   </p>
                 </div>
--- frontend/src/pages/Designs.jsx
+++ frontend/src/pages/Designs.jsx
@@ -49,17 +49,17 @@ const Designs = () => {
     },
   ];
   return (
-    <div>
+    <div className="">
       <NavBar />
 
       {/* Search and Functions */}
-      <div className="flex justify-center items-center gap-5">
+      <div className="flex justify-center items-center gap-5 my-8">
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512"
             width={30}
-            className="fill-blue"
+            className="fill-blue cursor-pointer"
           >
             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
           </svg>
@@ -69,7 +69,7 @@ const Designs = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width={35}
-            className="fill-blue"
+            className="fill-blue cursor-pointer"
             viewBox="0 0 24 24"
           >
             <path d="M21 3H5a1 1 0 0 0-1 1v2.59c0 .523.213 1.037.583 1.407L10 13.414V21a1.001 1.001 0 0 0 1.447.895l4-2c.339-.17.553-.516.553-.895v-5.586l5.417-5.417c.37-.37.583-.884.583-1.407V4a1 1 0 0 0-1-1zm-6.707 9.293A.996.996 0 0 0 14 13v5.382l-2 1V13a.996.996 0 0 0-.293-.707L6 6.59V5h14.001l.002 1.583-5.71 5.71z"></path>
@@ -77,11 +77,15 @@ const Designs = () => {
         </div>
       </div>
 
-      <div className=" grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-y-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-y-12 mx-8 md:mx-52 items-center justify-center">
+
         {info.map(({ id, title, description, img }) => (
-          <div key={id} className="justify-center items-center">
-            <Thumbnail img={img} title={title} description={description} />
-          </div>
+          <Thumbnail
+            key={id}
+            img={img}
+            title={title}
+            description={description}
+          />
         ))}
       </div>
     </div>
